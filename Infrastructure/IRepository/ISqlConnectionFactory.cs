64b7dfe9bef95f9e06275d22753954885081d7b3
amir <amirlesani77@gmail.com>
2026-08-07 17:34:37 +0330

    adding migration and sqlconnection factory

---
+++ Infrastructure/IRepository/ISqlConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Infrastructure.IRepository
+{
+    public interface ISqlConnectionFactory
+    {
+        IDbConnection CreateConnection(string connectionString);
+    }
+}
---
+++ Infrastructure/Repository/SqlConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.IRepository;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly string? _connnectionString;
+        public SqlConnectionFactory(string? connnectionString)
+        {
+            _connnectionString = connnectionString;
+        }
+
+        public IDbConnection CreateConnection(string connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
